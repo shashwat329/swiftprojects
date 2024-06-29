53a677d1dbe9cafe1cf2465ceaa922b2c8153c28
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-06-30 04:58:13 +0530

    APIService

---
+++ weatherApp/weatherApp/APIService.swift
@@ -0,0 +1,40 @@
+import Foundation
+class APIService{
+    public static let shared = APIService()
+    
+    public enum APIError: Error{
+        case error(_ errorString: String)
+    }
+    
+    public func getJSON<T: Decodable>(urlString: String,
+                                      dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate,
+                                      keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys,
+                                      compeletion: @escaping (Result<T,APIError>) ->Void){
+        
+        guard let url = URL(string: urlString) else{
+            compeletion(.failure(.error(NSLocalizedString("Error: invaild URL", comment: ""))))
+            return
+        }
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request ){(data,response,error) in
+            if let error = error{
+                compeletion(.failure(.error("Error:\(error.localizedDescription)")))
+                return
+            }
+            guard let data = data else{
+                compeletion(.failure(.error(NSLocalizedString("Error : data us corrupt.", comment: ""))))
+                return
+            }
+            let decoder = JSONDecoder()
+            do{
+                let decodedData = try decoder.decode(T.self, from: data)
+                compeletion(.success(decodedData))
+                return
+            } catch let decodingError{
+                compeletion(.failure(APIError.error("Error: \(decodingError.localizedDescription)")))
+                return
+            }
+            
+        }.resume()
+    }
+}
