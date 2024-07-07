5acb2fee918ded2bc8f05186630c6a09dfb325ae
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-07-07 14:10:34 +0530

    added new properties

--- weatherApp/weatherApp/ContentView.swift
+++ weatherApp/weatherApp/ContentView.swift
@@ -4,12 +4,68 @@
 //
 //  Created by shashwat singh on 07/07/24.
 //
-
+import CoreLocation
 import SwiftUI
 
 struct ContentView: View {
+    @State private var location: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Enter location: ", text: $location)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button {
+                        getweatherForecast(for: location)
+                    } label: {
+                        Image(systemName: "magnifyingglass.circle.fill")
+                            .font(.title3)
+                    }
+                }
+                Spacer()
+            }
+            .padding(.horizontal)
+            .navigationTitle("Mobile weather")
+        }
+    }
+    
+    func getweatherForecast(for location: String) {
+        let apiService = APIService.shared
+        CLGeocoder().geocodeAddressString(location) { (placemarks, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            if let lat = placemarks?.first?.location?.coordinate.latitude,
+               let lon = placemarks?.first?.location?.coordinate.longitude {
+                let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=b09a396edc775c0707100be5c860b58f"
+                apiService.getJSON(urlString: urlString, dateDecodingStrategy: .secondsSince1970) { (result: Result<Forecast, APIService.APIError>) in
+                    switch result {
+                    case .success(let forecast):
+                        for weather in forecast.weather {
+                                               print("Description: \(weather.description)")
+                                           print("Icon URL: \(weather.weatherIconURL)")
+                                           }
+                                           print("Temperature: \(forecast.main.temp)")
+                                          print("Max Temp: \(forecast.main.temp_max)")
+                                          print("Min Temp: \(forecast.main.temp_min)")
+                                           print("Pressure: \(forecast.main.pressure)")
+                                          print("Humidity: \(forecast.main.humidity)")
+                                           print("Visibility: \(forecast.visibility)")
+                                          print("Wind Speed: \(forecast.wind.speed)")
+                                           print("Country: \(forecast.sys.country)")
+                                          print("Sunrise: \(forecast.sys.sunrise)")
+                                           print("Sunset: \(forecast.sys.sunset)")
+                    case .failure(let apiError):
+                        switch apiError {
+                        case .error(let errorString):
+                            print(errorString)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
--- weatherApp/weatherApp/Forecast.swift
+++ weatherApp/weatherApp/Forecast.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+struct Forecast: Codable{
+    struct Weather: Codable{
+        let description: String
+        let icon: String
+        var weatherIconURl: URL{
+            let urlstring = "https://openweathermap.org/img/wn/\(icon)@2x.png"
+            return URL(string:urlstring)!
+        }
+    }
+    let weather: [Weather]
+    struct Main: Codable{
+        let temp: Double
+        let max_temp: Double
+        let min_temp: Double
+        let pressure: Int
+        let humidity: Int
+    }
+    let main:[Main]
+    let visibility: Int
+    struct Wind: Codable{
+        let speed: Double
+    }
+    let wind : [Wind]
+    let dt: Data
+    struct Sys: Codable{
+        let country : String
+        let sunrise: Int
+        let sunset:Int
+    }
+    let sys: [Sys]
+}
