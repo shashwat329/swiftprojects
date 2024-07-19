26c00097a03118a8d88ffe714b5031721aa1cf2b
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-07-19 23:44:38 +0530

    added navgation

--- signup_login/signup_login/Start.swift
+++ signup_login/signup_login/Start.swift
@@ -10,33 +10,30 @@ struct Start: View {
     @State var nextpage: Bool = false
     var body: some View {
         ZStack{
-            
-            VStack{
-                Spacer()
-                Text("Logo".uppercased())
-                    .font(.largeTitle)
-                    .bold()
-                    .foregroundColor(.orange)
-                Spacer()
-                Button(action: {
-                    nextpage.toggle()
-                },
-                       label: {
-                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                        .fill(Color(hex: "#1A3636"))
-                        .frame(width: 297,height: 55)
-                        .overlay{
-                            Text("get started".capitalized)
-                                .foregroundColor(.white)
-                        }
-                    
-                })
-                if nextpage{
-                    login()
-                    .transition(.move(edge: .bottom))
+            NavigationView{
+                VStack{
+                    Spacer()
+                    Text("Logo".uppercased())
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(.orange)
+                    Spacer()
+                    NavigationLink(destination: login()){
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(hex: "#1A3636"))
+                            .frame(width: 297,height: 55)
+                            .overlay{
+                                Text("get started".capitalized)
+                                    .foregroundColor(.white)
+                            }
+
+                    }
+                    Spacer()
                 }
-                Spacer()
+                
             }
+            .navigationBarHidden(true)
+            
         }
     }
     
