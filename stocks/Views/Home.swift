50dd2cf700d651c135bd7c6c039ade6e30ddbda2
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-10-13 23:22:21 +0530

    started stocks app

--- stocks/Views/Home.swift
+++ stocks/Views/Home.swift
@@ -7,12 +7,73 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Text("logo")
+                    .font(.largeTitle)
+                    .foregroundStyle(Color.orange)
+                    .padding()
+                
+                Spacer()
+                HStack(){
+                    Image(systemName: "magnifyingglass")
+                        .font(.largeTitle)
+                    Image(systemName: "person.circle")
+                        .font(.largeTitle)
+                }
+                .padding()
+            }
+            .background(Color.gray.opacity(0.1))
+//            .border(Color.black,width: 1)
+            Spacer()
+            HStack{
+                VStack(alignment:.leading){
+                    Text("Top Stocks")
+                        .font(.title)
+                        .padding(5)
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack{
+                            ForEach(0..<10, id: \.self) { index in
+                                Rectangle()
+                                    .fill(Color.gray.opacity(0.1))
+                                    .frame(width: 210,height: 210)
+                                    .overlay {
+                                        Text("stock \(index)")
+                                    }
+                            }
+                        }
+                    }
+                }
+                
+            }
+            Spacer()
+            HStack{
+                VStack(alignment:.leading){
+                    Text("Indices ")
+                        .font(.title)
+                        .padding(5)
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack{
+                            ForEach(0..<10, id: \.self) { index in
+                                Rectangle()
+                                    .fill(Color.gray.opacity(0.1))
+                                    .frame(width: 210,height: 100)
+                                    .overlay {
+                                        Text("stock index \(index)")
+                                    }
+                            }
+                        }
+                    }
+                }
+                
+            }
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    Home()
 }
